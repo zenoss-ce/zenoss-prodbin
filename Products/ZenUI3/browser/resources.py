a01c6b98edade277cc268a0af144d093c6fb0bb5
John Matos <rDr4g0n@gmail.com>
2016-05-17 15:47:08 -0500

    adhoc graph demo 1

--- Products/ZenUI3/browser/resources.py
+++ Products/ZenUI3/browser/resources.py
@@ -1,10 +1,10 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2009, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -14,23 +14,23 @@ import json
 import logging
 import Globals
 from zope.interface import implements
-from Products.Five.browser import BrowserView
+from Products.Five.browser import BrowserView, pagetemplatefile
 from Products.Five.viewlet.manager import ViewletManagerBase
 from Products.ZenUI3.browser.interfaces import IHeadExtraManager
 
 _MISSING_JS_FILE_MESSAGE="""
 ************************************************************
 The compressed javascript file %s
-does not exist.  Zenoss will still run but UI performance is 
-HEAVILY DEGRADED.  Please fix by running inst/buildjs.sh. 
+does not exist.  Zenoss will still run but UI performance is
+HEAVILY DEGRADED.  Please fix by running inst/buildjs.sh.
 ************************************************************"""
 
 def _checkForCompiledJSFile():
-    COMPILED_JS_FILE = os.path.join(os.path.dirname(__file__), 
+    COMPILED_JS_FILE = os.path.join(os.path.dirname(__file__),
                                 'resources/js/deploy/zenoss-compiled.js')
     jsFileExists =  os.path.exists(COMPILED_JS_FILE)
     if not Globals.DevelopmentMode and not jsFileExists:
-        logging.getLogger().warning(_MISSING_JS_FILE_MESSAGE 
+        logging.getLogger().warning(_MISSING_JS_FILE_MESSAGE
                                     % COMPILED_JS_FILE)
     return jsFileExists
 
@@ -114,3 +114,11 @@ class HeadExtraManager(ViewletManagerBase):
     Simple viewlet manager allowing people to plug into <head>.
     """
     implements(IHeadExtraManager)
+
+class AdHocGraphs(BrowserView):
+    """
+    hi
+    """
+    def __call__(self):
+        t = pagetemplatefile.ViewPageTemplateFile('adhocGraphs.pt')
+        return t(self)
